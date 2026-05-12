2d43d270ab986df20fa6051050e152307bc52bda
mahucom9 <mahucom9@gmail.com>
2026-05-12 19:32:57 -0300

    AUla12

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RpgApi.Data;
+
+namespace RpgApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public
+
+        private readonly ILogger<UsuariosController> _logger;
+
+        public UsuariosController(ILogger<UsuariosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
